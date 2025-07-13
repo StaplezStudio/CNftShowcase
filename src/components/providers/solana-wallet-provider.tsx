e9c7639eeb738018ecb01f601d3e358a32b9af7b
Staplez Jay <staplezstudios@gmail.com>
2025-07-13 01:12:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/providers/solana-wallet-provider.tsx
+++ src/components/providers/solana-wallet-provider.tsx
@@ -4,10 +4,8 @@ import React, { useMemo, type FC, type ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { clusterApiUrl } from '@solana/web3.js';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
