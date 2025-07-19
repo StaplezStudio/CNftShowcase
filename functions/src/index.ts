1c62b2826ccf2cd0d391367f7a35492ae4247f67
Staplez Jay <staplezstudios@gmail.com>
2025-07-19 03:52:53 +0000

    Error: Non-base58 character     at Object.decode (C:\Users\worke\OneDriv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -271,5 +271,3 @@ export const createCancelListingTransaction = onCall<CancelData>({ cors: true },
         throw new HttpsError("internal", "Could not create the cancel instruction.", { message: error.message });
     }
 });
-
-    
